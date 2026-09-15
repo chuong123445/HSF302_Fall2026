466d397dba6f66f9ac5cd40f8b29f73ddb0163a9
Nguyen Van Chuong <chuongaln@gmail.com>
2026-09-15 16:33:05 +0700

    feat(Todo 04): Create JPAUtil.java and update Main.java for testing -- slot 3

--- Slot 3/Chapter1_JPA_MappingOneToMany/src/main/java/fu/DE200388/Main.java
+++ Slot 3/Chapter1_JPA_MappingOneToMany/src/main/java/fu/DE200388/Main.java
@@ -1,17 +1,37 @@
 package fu.DE200388;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import fu.DE200388.Pojo.Department;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import fu.DE200388.Pojo.Department;
+import fu.DE200388.Pojo.Employee;
+import fu.DE200388.Pojo.Gender;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Department dept = new Department("IT");
+
+        Employee emp = new Employee(
+                "Test",
+                "test@company.com",
+                new BigDecimal("1000"),
+                Gender.OTHER,
+                true,
+                LocalDate.now()
+        );
+
+        dept.addEmployee(emp);
+
+        System.out.println(
+                dept.getEmployees().contains(emp)
+        );
+
+        System.out.println(
+                emp.getDepartment() == dept
+        );
     }
 }
